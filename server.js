4ba338fcc2c170e57c7d55a01eaa17f053b783e3
aLEX <yesitlabs.vishwa@gmail.com>
2022-06-23 11:36:50 +0530

    Made schema and rectified apis rotes, apilist file

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = 3000 || process.env.port;
+const connection= require('./config/default');
+const path = require('path');
+connection();
 
+app.use(express.json());
+
+app.use('/api/createItem',require('./routes/api/apilist'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => {
   console.log(`App started on ${PORT}`);
 });
